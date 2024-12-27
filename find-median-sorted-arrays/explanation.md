8467ec7a65948beeed64a2e46a7b2050194766b4
cloudflips32 <devadamstandish@gmail.com>
2024-12-26 22:46:54 -0500

    findMedianSortedArrays completed

---
+++ find-median-sorted-arrays/explanation.md
@@ -0,0 +1,8 @@
+# '''Find the Median of the Sorted Arrays!'''
+
+Your input: 2 number arrays
+Your output: the median of the two arrays
+
+Given the two sorted arrays of varied sizes, return the median of the two combined arrays.
+
+![examples-and-constraints](examples-constraints.png)
---
+++ find-median-sorted-arrays/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+function findMedianSortedArrays(nums1, nums2){
+  let merged = nums1.concat(nums2);
+  merged.sort((a, b) => a - b);
+    
+  let mid = Math.floor(merged.length / 2);
+  if (merged.length % 2 === 0){
+      return console.log((merged[mid-1] + merged[mid]) / 2);
+  } else {
+      return console.log(merged[mid]);
+  }
+};
+
+findMedianSortedArrays([1,3],[2]);
+findMedianSortedArrays([1,2],[3,4]);
+findMedianSortedArrays([1,2,3,4,5],[6,7,8,9,10,11,12,13,14,15,16,17]);
